f08e63746db4f70249fcda5fcb69a168b9f5e8c9
nuyiwa908 <maoja37@gmail.com>
2021-11-30 02:41:15 +0100

    new commit

---
+++ lib/Screens/DashboardScreens/add_new_product.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class NewProductScreen extends StatefulWidget {
+  const NewProductScreen({ Key? key }) : super(key: key);
+
+  @override
+  _NewProductScreenState createState() => _NewProductScreenState();
+}
+
+class _NewProductScreenState extends State<NewProductScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
